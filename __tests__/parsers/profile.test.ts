b8d8401beeb609503dcebd38847b1b86e8fcb36e
Justin Williams <justinw@me.com>
2023-05-17 07:08:47 -0600

    WIP on profile parsing

---
+++ __tests__/parsers/profile.test.ts
@@ -0,0 +1,19 @@
+describe('Parsing profiles', () => {
+  it('should pass', () => {
+    expect(true).toBe(true)
+  })
+
+  //   it('parse a user profile', async () => {
+  //     const profile = await parseProfile('https://staging.bsky.app/profile/chinchillazilla.hellthread.vet')
+  //     expect(profile).toBeDefined()
+  //   })
+  //   it('should fail to parse a non-profile URL', async () => {
+  //     try {
+  //       await parseProfile('https://staging.bsky.app/profile/chinchillazilla.hellthread.vet/post/3jvhqs4j6kw2n')
+  //       expect(true).toBe(false)
+  //     } catch (e) {
+  //       const error = e as Error
+  //       expect(error.message).toBe('Not a profile URL')
+  //     }
+  //   })
+})
