deb43c7a7532c0a9d367930ba49eb81eb8789458
Jeremy Bunting <187095+qbunt@users.noreply.github.com>
2023-05-19 15:34:59 -0400

    malformed the viteconfig

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   test: {
     includeSource: ['__tests__/**/*.ts'],
-  },
-  coverage: {
+    coverage: {
       // you can include other reporters, but 'json-summary' is required, json is recommended
       reporter: ['text', 'json-summary', 'json'],
-  }
+    },
+  },
 })
