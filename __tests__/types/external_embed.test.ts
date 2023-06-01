fc554a28f45789f184c20d875a7286f2ee00a090
Justin Williams <justinw@me.com>
2023-06-01 13:54:39 -0600

    Add missing tests for quoted posts and external embeds

---
+++ __tests__/types/external_embed.test.ts
@@ -0,0 +1,42 @@
+import { ExternalEmbed } from '../../src/types/external_embed'
+import * as BlueSky from '@atproto/api'
+
+describe('ExternalEmbed', () => {
+  describe('constructor', () => {
+    it('should create an instance from a BlueSky external', () => {
+      const external: BlueSky.AppBskyEmbedExternal.ViewExternal = {
+        uri: 'https://example.com',
+        title: 'Example',
+        description: 'An example',
+        thumb: 'https://example.com/thumb.png',
+      }
+      const embed = new ExternalEmbed(external)
+      expect(embed.uri).toEqual(external.uri)
+      expect(embed.title).toEqual(external.title)
+      expect(embed.description).toEqual(external.description)
+    })
+
+    it('should create an instance from individual properties', () => {
+      const uri = 'https://example.com'
+      const title = 'Example'
+      const description = 'An example'
+      const thumb = 'https://example.com/thumb.png'
+      const embed = new ExternalEmbed(uri, title, description, thumb)
+      expect(embed.uri).toEqual(uri)
+      expect(embed.title).toEqual(title)
+      expect(embed.description).toEqual(description)
+      expect(embed.thumb).toEqual(new URL(thumb))
+    })
+
+    it('should create an instance without a thumb', () => {
+      const uri = 'https://example.com'
+      const title = 'Example'
+      const description = 'An example'
+      const embed = new ExternalEmbed(uri, title, description)
+      expect(embed.uri).toEqual(uri)
+      expect(embed.title).toEqual(title)
+      expect(embed.description).toEqual(description)
+      expect(embed.thumb).toBeUndefined()
+    })
+  })
+})
